8603a22b4c06f031e289e4df15e8453cdeaa4ff6
RaulJCS5 <rauljosesantos@hotmail.com>
2023-03-10 11:18:27 +0000

    Commit: Add MUI and dark mode state

--- rjcs-app/src/App.tsx
+++ rjcs-app/src/App.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import { useMemo,useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Projects } from "./pages/Projects";
 import { About } from "./pages/About";
@@ -6,21 +6,39 @@ import { Contact } from "./pages/Contact";
 import { Home } from "./pages/Home";
 import { Layout } from "./pages/Layout";
 import { NoPage } from "./pages/NoPage";
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+  function toggleColorMode(){
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout toggleColorMode={toggleColorMode} theme={theme} />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- rjcs-app/src/pages/Layout.tsx
+++ rjcs-app/src/pages/Layout.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Outlet, Link } from "react-router-dom";
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { Theme } from '@mui/material';
 
-export const Layout = () => {
+
+export const Layout = ({toggleColorMode,theme}:{toggleColorMode:()=>void,theme:Theme}) => {
     return (
         <div>
             <nav>
@@ -9,6 +14,9 @@ export const Layout = () => {
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
                 <Link to="/projects">Projects</Link>
+                <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
+                    {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                </IconButton>
             </nav>
             <Outlet />
         </div>
