f5f68e8ce172cd7d36cd49d428b2e1de3456b642
RaulJCS5 <rauljosesantos@hotmail.com>
2023-03-12 14:04:08 +0000

    Commit: About and Social Icons

--- rjcs-app/src/pages/About.tsx
+++ rjcs-app/src/pages/About.tsx
@@ -6,8 +6,9 @@ import Typography from '@mui/material/Typography';
 export const About = () => {
   return (
     <Box sx={{ textAlign: 'center' }}>
-        <Typography variant='h4'>About</Typography>
-        <Typography variant='h6'>Hi, I am a software developer 👨‍💻 with a passion ❤ for creating innovative and efficient solutions. I have a firm background in various programming languages and technologies, and I am skilled in front-end and back-end development. I am comfortable taking on new challenges and learning new technologies. I am always eager to learn and grow as a software developer. But when I'm not in front of a computer, I love to blow off steam by playing football⚽ or getting out in nature🌳 for some challenging outdoor exercise💪 also I love swimming🏊‍♂️, and just being near the water🌊 I feel a deep sense of joy and connection.</Typography>
+      <Typography variant='h4'>About</Typography>
+      <Typography variant='h6'>Hello, I am a software developer excited to create innovative and efficient solutions. I have a solid background in various programming languages and technologies and talents in front-end and back-end development. I am comfortable taking on new challenges and learning new technologies.</Typography>
+      <Typography variant='h6'>I am always eager to learn and grow as a software developer. When I'm not in front of a computer, I have a deep sense of joy and connection with the water, so I like to do water sports such as swimming and cliff jumping in the water, and my hobbies are playing football or getting out in nature for some challenging hiking or others outdoor exercise.</Typography>
     </Box>
   )
 }
--- rjcs-app/src/pages/Contact.tsx
+++ rjcs-app/src/pages/Contact.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography';
+import { SocialIcon } from 'react-social-icons';
 
 export const Contact = () => {
   return (
     <Box sx={{ textAlign: 'center' }}>
       <Typography variant='h4'>Contact</Typography>
-      <Typography variant='h6'>Email: <a href="mailto:rauljosesantos@hotmail.com" target="blank" rel="noopener">rauljosesantos@hotmail.com</a></Typography>
-      <Typography variant='h6'>LinkedIn: <a href="https://www.linkedin.com/in/rauljosecsantos/" target="blank" rel="noopener">linkedin.com/in/rauljosecsantos/</a></Typography>
-      <Typography variant='h6'>GitHub: <a href="https://github.com/RaulJCS5" target="blank" rel="noopener">github.com/RaulJCS5</a></Typography>
-      <Typography variant='h6'>Website: <a href="https://rauljcs5.github.io/" target="blank" rel="noopener">rauljcs5.github.io/</a></Typography>
+      <a href="mailto:rauljosesantos@hotmail.com" target="blank" rel="noopener"><SocialIcon network='email'></SocialIcon></a>
+      <SocialIcon url='https://www.linkedin.com/in/rauljosecsantos/'></SocialIcon>
+      <SocialIcon url='https://twitter.com/rauljosesan'></SocialIcon>
+      <SocialIcon url='https://www.instagram.com/rauljcsantos'></SocialIcon>
+      <SocialIcon url='https://www.facebook.com/raul.jose.35/'></SocialIcon>
+      <SocialIcon url='https://soundcloud.com/user-692546360'></SocialIcon> 
+      <SocialIcon url='https://github.com/RaulJCS5'></SocialIcon>
+      <SocialIcon url='https://discordapp.com/users/368011779760193537'></SocialIcon>
+      <SocialIcon url='https://rauljcs5.github.io/' network='sharethis'></SocialIcon>
     </Box>
   )
 }
