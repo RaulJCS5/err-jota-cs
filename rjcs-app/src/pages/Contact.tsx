c1a552cbac1ced83b5ab8d9a8f78eac9c5d9bb2d
RaulJCS5 <rauljosesantos@hotmail.com>
2023-03-13 17:26:05 +0000

    Commit: Correct Contact

--- rjcs-app/src/pages/Contact.tsx
+++ rjcs-app/src/pages/Contact.tsx
@@ -11,23 +11,18 @@ export default function ActionAreaCard() {
   return (
     <Card sx={{ maxWidth: 600, borderRadius: 5 }}>
       <CardContent>
-        <Box sx={{ textAlign: 'center' }}>
-          <Typography sx={{ marginBottom: 5 }} variant='h4'>Also you can find me on</Typography>
-          <Grid container spacing={1}>
-            <Grid item>
-              <a href="mailto:rauljosesantos@hotmail.com" target="blank" rel="noopener"><SocialIcon bgColor={changeBG()} fgColor={changeFG()} network='email'></SocialIcon></a>
-            </Grid>
-            <Grid item>
-              <SocialIcon bgColor={changeBG()} fgColor={changeFG()} url='https://soundcloud.com/user-692546360'></SocialIcon>
-            </Grid>
-            <Grid item>
-              <SocialIcon fgColor='white' url='https://discordapp.com/users/368011779760193537'></SocialIcon>
-            </Grid>
-            <Grid item>
-              <SocialIcon bgColor={changeBG()} fgColor={changeFG()} url='https://rauljcs5.github.io/' network='sharethis'></SocialIcon>
-            </Grid>
+        <Typography sx={{ marginBottom: 5 }} variant='h4'>Also you can find me on</Typography>
+        <Grid sx={{display: 'flex', justifyContent: 'center'}} container spacing={1}>
+          <Grid item>
+            <a href="mailto:rauljosesantos@hotmail.com" target="blank" rel="noopener"><SocialIcon bgColor={changeBG()} fgColor={changeFG()} network='email'></SocialIcon></a>
           </Grid>
-        </Box>
+          <Grid item>
+            <SocialIcon bgColor={changeBG()} fgColor={changeFG()} url='https://soundcloud.com/user-692546360'></SocialIcon>
+          </Grid>
+          <Grid item>
+            <SocialIcon bgColor={changeBG()} fgColor={changeFG()} url='https://rauljcs5.github.io/' network='sharethis'></SocialIcon>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
@@ -38,7 +33,6 @@ export const Contact = () => {
     <Box sx={{
       display: "flex",
       justifyContent: "center",
-      alignItems: "center",
     }}>
       <ActionAreaCard></ActionAreaCard>
     </Box>
